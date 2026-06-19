741b31185d0861ca080f5eb8458752a3211adc33
compiler-exe <roopeshdoragadum@gmail.com>
2026-06-19 18:49:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (34.70%)

---
+++ my-solutions/0209-minimum-size-subarray-sum/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left=0;
+        int currentSum=0;
+        int minLength = INT_MAX;
+        for(int right=0;right< nums.size();right++){
+            currentSum += nums[right];
+            while(currentSum >= target){
+                minLength = min(minLength , right - left +1 );
+                currentSum = currentSum - nums[left];
+                left++;
+            }
+        }
+        return minLength == INT_MAX ? 0 : minLength;
+    }
+};
