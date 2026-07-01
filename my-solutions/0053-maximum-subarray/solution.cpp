df0ca94a85bcb48d3621b7b598baa3fb14b05799
compiler-exe <roopeshdoragadum@gmail.com>
2026-07-01 09:22:55 +0000

    Sync LeetCode submission Runtime - 1 ms (25.04%), Memory - 71.8 MB (15.18%)

--- my-solutions/0053-maximum-subarray/solution.cpp
+++ my-solutions/0053-maximum-subarray/solution.cpp
@@ -4,21 +4,14 @@
 class Solution {
 public:
     int maxSubArray(std::vector<int>& nums) {
-        int max_sum = INT_MIN;
-        int current_sum = 0;
-        int left = 0;
-
-        for (int right = 0; right < nums.size(); ++right) {
-            current_sum += nums[right];
-            
-            max_sum = std::max(max_sum, current_sum);
-
-            while (current_sum < 0 && left <= right) {
-                current_sum -= nums[left];
-                left++;
-            }
-        };
-
-        return max_sum;
+        long long  maxSum = INT_MIN;
+        long long  currentSum =0;
+        for(int i=0;i<nums.size();i++){
+            long long  v1 = nums[i];
+            long long v2 = currentSum + nums[i];
+            currentSum = max( v1 ,v2);
+            maxSum = max(maxSum , currentSum);
+        }
+        return maxSum;
     }
 };
