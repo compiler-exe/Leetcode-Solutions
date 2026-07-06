9f337f44c8504c0c2e280e3518c315ad5d1704ad
compiler-exe <roopeshdoragadum@gmail.com>
2026-07-06 08:45:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (27.81%)

---
+++ my-solutions/1222-remove-covered-intervals/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        int removed_count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue; 
+                }
+                if (intervals[j][0] <= intervals[i][0] && intervals[j][1] >= intervals[i][1]) {
+                    removed_count++;
+                    break; 
+                }
+            }
+        }
+        return n - removed_count;
+    }
+};
