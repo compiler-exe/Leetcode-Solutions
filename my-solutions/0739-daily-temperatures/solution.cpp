728f7ff4340cebd81ac5a08cf80e72ba9e4f440c
compiler-exe <roopeshdoragadum@gmail.com>
2026-07-12 18:34:19 +0000

    Sync LeetCode submission Runtime - 20 ms (63.53%), Memory - 107.4 MB (34.63%)

---
+++ my-solutions/0739-daily-temperatures/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int>result(n,0);
+        stack<int>st;
+        for(int i =0;i<n;++i){
+            while(!st.empty() && temperatures[st.top()]< temperatures[i]){
+                int prev_index = st.top();
+                st.pop();
+                result[prev_index]=i-prev_index;
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
