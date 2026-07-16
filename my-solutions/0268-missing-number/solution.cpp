41d2848e5b7495411a07939228dd7bd85aeb59e2
compiler-exe <roopeshdoragadum@gmail.com>
2026-07-16 07:45:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.7 MB (71.51%)

---
+++ my-solutions/0268-missing-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = (n*(n+1))/2;
+        int found =0;
+        for(int i=0;i<n;i++){
+            found += nums[i];
+        }
+        return sum - found;
+    }
+};
