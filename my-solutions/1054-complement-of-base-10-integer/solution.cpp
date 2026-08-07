1bbbe4ec01422a09677e3e632559b92e8b65df0b
compiler-exe <roopeshdoragadum@gmail.com>
2026-08-07 18:13:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (92.61%)

---
+++ my-solutions/1054-complement-of-base-10-integer/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        unsigned int mask = ~0;
+        while (mask & n) {
+            mask <<= 1;
+        }
+        return ~mask ^ n;
+    }
+};
